da0466f056f45d4c1c17835c7b199dd1ae296eb8
junwen_su <517897442@qq.com>
2017-05-01 23:24:21 +0800

    fix bug

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -4,7 +4,7 @@
     <v-title-bar v-if="newsContent != null" :content="newsContent.image_source" :shareUrl="newsContent.share_url" :backFunc="clickBack"></v-title-bar>
     <!-- <v-content v-if="newsContent != null"> -->
       <div class="img_div" v-if="newsContent != null">
-        <img :src="'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='+newsContent.image" alt="">
+        <img v-if="titleImg !== null" :src="'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='+titleImg" alt="">
         <h3>{{newsContent.title}}</h3>
       </div>
       <div class="html_content" v-html="newsContent.body" v-if="newsContent != null">
@@ -26,7 +26,8 @@ export default {
       },
       isLoading: false,
       isError: false,
-      linkcss: null
+      linkcss: null,
+      titleImg: null
     }
   },
   methods: {
@@ -36,6 +37,7 @@ export default {
       let _this = this;
       api.getNewsById(id).then(function(data) {
         _this.newsContent = data.data;
+        _this.titleImg = data.data.image || null;
         if (_this.newsContent.css.length != 0 && _this.newsContent.css[0] != null) {
           let _link = document.createElement("link");
           _link.setAttribute("rel", "stylesheet");
@@ -52,7 +54,7 @@ export default {
         _this.isError = true;
       })
     },
-    clickBack(){
+    clickBack() {
       let _headTag = document.getElementsByTagName("head")[0];
       _headTag.removeChild(this.linkcss);
       this.$router.go(-1);
@@ -71,27 +73,28 @@ export default {
 .img_div {
     width: 100%;
     position: relative;
+    margin-bottom: 15px;
     img {
         width: 100%;
     }
     h3 {
-      display:flex;
-      justify-content: center;
-      align-items: center;
-      margin:0;
-      position: absolute;
-      bottom:0;
-      padding:8px 5px 0;
-      color:white;
-      font-size:17px;
-      font-family: "微软雅黑";
-      width:100%;
-      background:linear-gradient(to bottom, rgba(0,0,0,0.1) 0%, rgba(0,0,0,0.7) 95%);
-      height: 46px;
-      }
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0;
+        position: absolute;
+        bottom: 0;
+        padding: 8px 5px 0;
+        color: white;
+        font-size: 17px;
+        font-family: "微软雅黑";
+        width: 100%;
+        background: linear-gradient(to bottom, rgba(0,0,0,0.1) 0%, rgba(0,0,0,0.7) 95%);
+        height: 46px;
+    }
 }
-.content{
-  font-size:16px !important;
+.content {
+    font-size: 16px !important;
 }
 .headline .img-place-holder {
     height: 0 !important;
